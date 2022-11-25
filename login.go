5a25a986801feb872bbf3a090b5380c6c2357194
yi-ge <a@wyr.me>
2022-11-26 01:24:39 +0800

    to: login request

--- login.go
+++ login.go
@@ -2,33 +2,40 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"net/http"
+	"strconv"
 
 	jsoniter "github.com/json-iterator/go"
 	"github.com/yi-ge/file-sync/utils"
 )
 
-func login(email string, password string, machineName string) (newUser bool, publicKey string, privateKey string) {
+func login(email string, password string, machineName string) (newUser bool, publicKey string, privateKey string, err error) {
 	requestURL := apiURL + "/device/add"
 	machineId := utils.GetMachineID()
-	verify := utils.GetSha1Str(password[:64])
-	publicKey = ""
-	privateKey = ""
+	verify := utils.GetSha1Str(password[:16])
+	rsaPrivateKeyPassword := password[32:48]
+	rsaPrivateEncryptPassword := password[16:32]
+	_, encryptedPrivKeyPEMBase64, publicKeyPEM, err := utils.GenerateRSAKeypairPEM(4096, rsaPrivateKeyPassword)
+	encryptedPrivateKeyStr, err := utils.AESCTREncrypt([]byte(encryptedPrivKeyPEMBase64), []byte(rsaPrivateEncryptPassword))
+	if err != nil {
+		return false, "", "", err
+	}
 	bodyMap := map[string]string{
 		"email":       email,
 		"machineId":   machineId,
 		"machineName": machineName,
 		"verify":      verify,
-		"publicKey":   publicKey,
-		"privateKey":  privateKey,
+		"publicKey":   publicKeyPEM.String(),
+		"privateKey":  string(encryptedPrivateKeyStr),
 	}
 	jsonBody, _ := jsoniter.Marshal(bodyMap)
 	resp, err := http.Post(requestURL, "application/json", bytes.NewBuffer(jsonBody))
 
 	if err != nil {
 		logger.Infof("GET request failed: %s\n", err)
-		return false, "", ""
+		return false, "", "", err
 	}
 
 	defer resp.Body.Close()
@@ -36,24 +43,29 @@ func login(email string, password string, machineName string) (newUser bool, pub
 
 	if err != nil {
 		logger.Infof("Body read failed: %s\n", err)
-		return false, "", ""
+		return false, "", "", err
 	}
 
-	status := jsoniter.Get(body, "status").ToString()
+	status := jsoniter.Get(body, "status").ToInt()
 
-	if status != "1" {
+	if status != 1 {
 		logger.Infof("Server out status error: %s\n", status)
-		return false, "", ""
+		return false, "", "", errors.New("Server out status error: " + strconv.Itoa(status))
 	}
 
-	secretData, err := utils.RsaDecryptWithSha1Base64(jsoniter.Get(body, "result").ToString(), publicKey)
+	// result := jsoniter.Get(body, "result")
+
+	privateKey = jsoniter.Get(body, "result", "privateKey").ToString()
+	publicKey = jsoniter.Get(body, "result", "publicKey").ToString()
+
+	secretData, err := utils.RsaDecryptWithSha1Base64(privateKey, publicKey)
 
 	if err != nil {
 		logger.Errorf("Secret decrypt error: %s\n", err)
-		return false, "", ""
+		return false, "", "", err
 	}
 
 	signBase64 := jsoniter.Get([]byte(secretData), "sign").ToString()
 
-	return true, signBase64, ""
+	return true, signBase64, "", err
 }
