6cd7caae17e5e0bdc19dfd472b260bb7ac342c9a
yi-ge <a@wyr.me>
2023-01-02 22:15:32 +0800

    to: file config

--- file.go
+++ file.go
@@ -14,6 +14,187 @@ import (
 	"github.com/yi-ge/file-sync/utils"
 )
 
+func addConfig(fileId string, path string, data Data) (jsoniter.Any, error) {
+	requestURL := apiURL + "/file/config"
+	machineId := utils.GetMachineID()
+
+	if machineId != data.MachineId {
+		return nil, errors.New("machineId does not match")
+	}
+
+	timestamp := time.Now().UnixNano() / 1e6
+
+	bodyMap := map[string]string{
+		"email":     utils.GetSha1Str(data.Email),
+		"machineId": machineId,
+		"action":    "add",
+		"fileId":    fileId,
+		"path":      path,
+		"attribute": "",
+		"timestamp": strconv.FormatInt(timestamp, 10),
+	}
+
+	var dataParams string
+	var keys []string
+	for k := range bodyMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		dataParams = dataParams + k + "=" + bodyMap[k] + "&"
+	}
+
+	dataParams += data.Verify
+	// fmt.Println(dataParams)
+	// ff := dataParams[0 : len(dataParams)-1]
+
+	privateKeyEncrypted, err := getPrivateKey()
+	if err != nil {
+		return nil, err
+	}
+
+	privateKeyHex, err := base64.RawURLEncoding.DecodeString(string(privateKeyEncrypted))
+	if err != nil {
+		return nil, err
+	}
+
+	decrypted, plaintextBytes, err := utils.AESMACDecryptBytes(privateKeyHex, data.RsaPrivateKeyPassword)
+
+	if err != nil || !decrypted {
+		return nil, errors.New("secret decrypt error: " + err.Error())
+	}
+
+	token, err := utils.RsaSignWithSha1HexPkcs1(dataParams, string(plaintextBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	bodyMap["token"] = base64.RawURLEncoding.EncodeToString(token)
+
+	jsonBody, err := jsoniter.Marshal(bodyMap)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(requestURL, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, errors.New("HTTP request failed: " + err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("HTTP request failed: " + resp.Status)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	// fmt.Println(string(body))
+
+	if err != nil {
+		return nil, err
+	}
+
+	status := jsoniter.Get(body, "status").ToInt()
+
+	if status != 1 {
+		msg := jsoniter.Get(body, "msg").ToString()
+		return nil, errors.New(msg)
+	}
+
+	res := jsoniter.Get(body, "result")
+
+	return res, nil
+}
+
+func removeConfig(fileId string, data Data) error {
+	requestURL := apiURL + "/file/config"
+	machineId := utils.GetMachineID()
+
+	if machineId != data.MachineId {
+		return errors.New("machineId does not match")
+	}
+
+	timestamp := time.Now().UnixNano() / 1e6
+
+	bodyMap := map[string]string{
+		"email":     utils.GetSha1Str(data.Email),
+		"machineId": machineId,
+		"fileId":    fileId,
+		"action":    "remove",
+		"attribute": "",
+		"timestamp": strconv.FormatInt(timestamp, 10),
+	}
+
+	var dataParams string
+	var keys []string
+	for k := range bodyMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		dataParams = dataParams + k + "=" + bodyMap[k] + "&"
+	}
+
+	dataParams += data.Verify
+	// fmt.Println(dataParams)
+	// ff := dataParams[0 : len(dataParams)-1]
+
+	privateKeyEncrypted, err := getPrivateKey()
+	if err != nil {
+		return err
+	}
+
+	privateKeyHex, err := base64.RawURLEncoding.DecodeString(string(privateKeyEncrypted))
+	if err != nil {
+		return err
+	}
+
+	decrypted, plaintextBytes, err := utils.AESMACDecryptBytes(privateKeyHex, data.RsaPrivateKeyPassword)
+
+	if err != nil || !decrypted {
+		return errors.New("secret decrypt error: " + err.Error())
+	}
+
+	token, err := utils.RsaSignWithSha1HexPkcs1(dataParams, string(plaintextBytes))
+	if err != nil {
+		return err
+	}
+
+	bodyMap["token"] = base64.RawURLEncoding.EncodeToString(token)
+
+	jsonBody, err := jsoniter.Marshal(bodyMap)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(requestURL, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return errors.New("HTTP request failed: " + err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		return errors.New("HTTP request failed: " + resp.Status)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	// fmt.Println(string(body))
+
+	if err != nil {
+		return err
+	}
+
+	status := jsoniter.Get(body, "status").ToInt()
+
+	if status != 1 {
+		msg := jsoniter.Get(body, "msg").ToString()
+		return errors.New(msg)
+	}
+
+	return nil
+}
+
 func listConfigs(data Data) (jsoniter.Any, error) {
 	requestURL := apiURL + "/file/configs"
 	machineId := utils.GetMachineID()
