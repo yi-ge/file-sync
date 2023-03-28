910174fb4de087d261ae03bf97bea292c4ba9629
yi-ge <a@wyr.me>
2023-03-28 21:10:36 +0800

    fix: github action config

--- utils/sha256_test.go
+++ utils/sha256_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGetSha256Str(t *testing.T) {
 	sha256 := GetSha256Str("123456")
-	if sha256 != "7c4a8d09ca3762af61e59520943dc26494f8941b9962fba4e8d64d8" {
+	if sha256 != "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92" {
 		t.Error("sha256 error")
 	}
 }
